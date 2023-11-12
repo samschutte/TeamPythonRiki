587737ba15f53569c4ab84a2fcc1bc269b9c04e9
Alex Aossey <aossey.alex@gmail.com>
2023-11-12 10:52:13 -0500

    added testing and tests for search_by_tags

---
+++ Riki/tests/readme.txt
@@ -0,0 +1,7 @@
+TO RUN TESTS YOU MUST FIRST:
+> change tests/config.py to point to the /tests/content and /tests/users directories
+> pip install pytest
+
+
+Then it can be run with the command:
+> pytest
---
+++ Riki/tests/test_search_by_tags.py
@@ -0,0 +1,58 @@
+import pytest
+import os
+from wiki import create_app
+
+# Initialize your app for testing
+@pytest.fixture
+def app():
+    directory = os.getcwd()
+    test_directory = os.path.join(directory, "tests")
+    app = create_app(test_directory)
+    app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False  # Disable CSRF protection for testing
+    return app
+
+def test_user_login_route(app):
+    client = app.test_client()
+
+    # Test authenticated feature
+    with client:
+        # Simulate signing in
+        client.post('/user/login/', data={'name': 'test_user', 'password': '1234'},follow_redirects=True)
+        
+        # Now, test an authenticated route
+        response = client.get('/search_by_tags', follow_redirects=True)
+        assert response.status_code == 200
+        assert b'tag1' in response.data
+        assert b'Search By Tags' in response.data
+        
+        # Test POST request to the search_by_tags route with valid form data
+        data = {'tag1':True}
+        response = client.post('/search_by_tags/', data=data, follow_redirects=True)
+        assert response.status_code == 200
+        assert b'Results for <i>tag1</i>' in response.data
+        assert b'Testing_page_1' in response.data
+        assert b'Testing_page_2' in response.data
+        assert b'Testing_page_3' in response.data
+
+        # Test POST request to the search_by_tags route with valid form data
+        data = {'tag1':True, 'tag2':True}
+        response = client.post('/search_by_tags/', data=data, follow_redirects=True)
+        assert response.status_code == 200
+        assert b'Results for <i>tag1, tag2</i>' in response.data
+        assert b'Testing_page_1' not in response.data
+        assert b'Testing_page_2' in response.data
+        assert b'Testing_page_3' in response.data
+
+                # Test POST request to the search_by_tags route with valid form data
+        data = {'tag1':True, 'tag2':True, 'tag3':True}
+        response = client.post('/search_by_tags/', data=data, follow_redirects=True)
+        assert response.status_code == 200
+        assert b'Results for <i>tag1, tag2, tag3</i>' in response.data
+        assert b'Testing_page_1' not in response.data
+        assert b'Testing_page_2' not in response.data
+        assert b'Testing_page_3' in response.data
+        
+
+
+
