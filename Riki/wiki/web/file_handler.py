a1c2fcd19634a4b80c5eb6309ef6a24d5104eec2
cardslinger <omega.male.79@gmail.com>
2023-11-27 14:34:21 -0500

    Uploads complete
    File uploads now work and display correctly. All I need is a couple of unit tests here. From system testing, everything works!!

--- Riki/wiki/web/file_handler.py
+++ Riki/wiki/web/file_handler.py
@@ -15,10 +15,10 @@ class FileHandler:
         if self.fileName.lower().endswith(('.png', '.jpg', '.jpeg')):
             handler = ImageHandler(self.originalFile, self.page)
             handler.saveFile()
-        elif self.fileName.lower().endswith(('.mp3', '.mp4')):
+        elif self.fileName.lower().endswith(('.mp4')):
             handler = VideoHandler(self.originalFile, self.page)
             handler.saveFile()
-        elif self.fileName.lower().endswith(('.doc', '.docx', '.pdf')):
+        elif self.fileName.lower().endswith(('.doc', '.docx', '.pdf', '.txt')):
             handler = DocumentHandler(self.originalFile, self.page)
             handler.saveFile()
         else:
@@ -29,14 +29,16 @@ class ImageHandler(FileHandler):
     basepath = ""
     path = ""
     markdown = ""
+    url = ""
     found = False
     def __init__(self, originalFile, page):
         self.originalFile = originalFile
         self.fileName = secure_filename(originalFile.filename)
         self.page = page
         self.basepath = os.getcwd().replace("\\", "/")
-        self.path = self.basepath + "/wiki/web/files/" + self.page + "/images/"
+        self.path = self.basepath + "/wiki/web/static/files/" + self.page + "/images/"
         self.markdown = self.basepath + "/content/" + self.page + ".md"
+        self.url = "/static/files/" + self.page + "/images/"
     def saveFile(self):
         fullPath = self.path + self.fileName
         os.makedirs(os.path.dirname(fullPath), exist_ok=True)
@@ -48,18 +50,18 @@ class ImageHandler(FileHandler):
         index = 0
         for entry in data:
             if entry.startswith("images:"):
-                data[index] = data[index].strip() + ", " + self.path + self.fileName + "\n"
+                data[index] = data[index].strip() + ", " + self.url + self.fileName + "\n"
                 self.found = True
                 break
             index += 1
         index = 0
         if self.found == False:
             for entry in data:
+                print(entry)
                 if entry in ['\n', '\r\n']:
-                    print(True)
-                    data[index] = "images: " + self.path + self.fileName + "\n"
-                break
-            index += 1
+                    data[index] = "images: " + self.url + self.fileName + "\n"
+                    break
+                index += 1
         with open(self.markdown, "r+") as markdown:
             markdown.writelines(data)          
 
@@ -68,35 +70,79 @@ class VideoHandler(FileHandler):
     basepath = ""
     path = ""
     markdown = ""
+    url = ""
+    found = False
     def __init__(self, originalFile, page):
         self.originalFile = originalFile
         self.fileName = secure_filename(originalFile.filename)
         self.page = page
         self.basepath = os.getcwd().replace("\\", "/")
-        self.path = self.basepath + "/wiki/web/files/" + self.page + "/videos/"
+        self.path = self.basepath + "/wiki/web/static/files/" + self.page + "/videos/"
         self.markdown = self.basepath + "/content/" + self.page + ".md"
+        self.url = "/static/files/" + self.page + "/videos/"
     def saveFile(self):
         fullPath = self.path + self.fileName
         os.makedirs(os.path.dirname(fullPath), exist_ok=True)
         with open(self.path + self.fileName, "wb") as newFile:
             newFile.write(self.originalFile.stream.read())
-        details = open(self.markdown)
-        
+        with open(self.markdown, "r") as markdown:
+            data = markdown.readlines()
+            markdown.close()
+        index = 0
+        for entry in data:
+            if entry.startswith("videos:"):
+                data[index] = data[index].strip() + ", " + self.url + self.fileName + "\n"
+                self.found = True
+                break
+            index += 1
+        index = 0
+        if self.found == False:
+            for entry in data:
+                print(entry)
+                if entry in ['\n', '\r\n']:
+                    data[index] = "videos: " + self.url + self.fileName + "\n\n"
+                    break
+                index += 1
+        with open(self.markdown, "r+") as markdown:
+            markdown.writelines(data)
+
 # Document handler
 class DocumentHandler(FileHandler):
     basepath = ""
     path = ""
     markdown = ""
+    url = ""
+    found = False
     def __init__(self, originalFile, page):
         self.originalFile = originalFile
         self.fileName = secure_filename(originalFile.filename)
         self.page = page
         self.basepath = os.getcwd().replace("\\", "/")
-        self.path = self.basepath + "/wiki/web/files/" + self.page + "/documents/"
+        self.path = self.basepath + "/wiki/web/static/files/" + self.page + "/documents/"
         self.markdown = self.basepath + "/content/" + self.page + ".md"
+        self.url = "/static/files/" + self.page + "/documents/"
     def saveFile(self):
         fullPath = self.path + self.fileName
         os.makedirs(os.path.dirname(fullPath), exist_ok=True)
         with open(self.path + self.fileName, "wb") as newFile:
             newFile.write(self.originalFile.stream.read())
-        details = open(self.markdown)
+        with open(self.markdown, "r") as markdown:
+            data = markdown.readlines()
+            markdown.close()
+        index = 0
+        for entry in data:
+            if entry.startswith("documents:"):
+                data[index] = data[index].strip() + ", " + self.url + self.fileName + "\n\n"
+                self.found = True
+                break
+            index += 1
+        index = 0
+        if self.found == False:
+            for entry in data:
+                print(entry)
+                if entry in ['\n', '\r\n']:
+                    data[index] = "documents: " + self.url + self.fileName + "\n\n"
+                    break
+                index += 1
+        with open(self.markdown, "r+") as markdown:
+            markdown.writelines(data)
